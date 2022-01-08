8810d0d1fc19737e0c4b3913ba02c1d6386b291c
sharifulislam565619 <sharifulislam565619@gmail.com>
2022-01-08 16:37:01 +0600

    payment method added

--- src/components/Dashboard/Payment/Payment.js
+++ src/components/Dashboard/Payment/Payment.js
@@ -1,9 +1,20 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
+import CheckoutForm from './CheckoutForm';
 
+const stripePromise = loadStripe('pk_test_51JvnHtJk7W4cFWOVYQs9UfJru8bMTh3Y5uEK5nXd9Npy5eGTjC4zfBwSgsGb5D5dHIvrsTZPP5M1YumigcGeBLOE00gt4kPA5C')
 const Payment = () => {
+   const options = {
+      clientSecret: '{{sk_test_51JvnHtJk7W4cFWOVQcSaRu5QROGDVJypp3kWeck03B5HJmvVSL2YFg9vZCnFJKzXunBHNV3wReRniXnZb931a7jG00FjWEtVur}}',
+   }
    return (
       <div>
-         <h2 style={{ fontStyle: "italic", marginTop: "100px", color: "#0ba75f" }} >Payment system coming soon...</h2>
+         <h2 style={{ fontStyle: "italic", marginTop: "100px", color: "#0ba75f" }} >Payment</h2>
+
+         <Elements stripe={stripePromise}>
+            <CheckoutForm />
+         </Elements>
       </div>
    );
 };
